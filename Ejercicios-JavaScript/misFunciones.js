ea3f493c2a828ed1e7f8459fb8bea93b987644fb
nombreUsuario <miEmail@domain.com>
2022-05-25 12:15:09 -0300

    se relizaron mas ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,34 +1,86 @@
 /**
  * convercion de unidades, metros, yardas, pies, y pulgadas
- * @method cambiar unidades
+ * @method cambiarUnidades
+ * @param   {string} id- El id de los imputs de metros, yardas, pies, pulgadas
+ * @param {number} valor valor de los imputs de metros yardas, pies ,o pulgadas
  * @return
- * @para    id de los imputs de metro, yardas, pies o pulgadas
- * @para -  el valr de lso imouts de metros yardas, pies , o pulgadas
  */
-function cambiarUnidades(id, valor){
-    if(isNaN(valor)){
-        alert("Se ingreso un valor invalido en " +id);
+function cambiarUnidades(id, valor) {
+    if (isNaN(valor)) {
+        alert("Se ingreso un valor invalido en " + id);
         document.lasUnidades.unid_metro.value = "";
         document.lasUnidades.unid_yarda.value = "";
         document.lasUnidades.unid_pie.value = "";
         document.lasUnidades.unid_pulgada.value = "";
-    }else if (id =="metro") {
+    } else if (id == "metro") {
         document.lasUnidades.unid_pulgada.value = valor * 39.3701;
         document.lasUnidades.unid_pie.value = valor * 3.28084;
         document.lasUnidades.unid_yarda.value = valor * 1.09361;
-   }else if (id=="pulgada"){
+    } else if (id == "pulgada") {
         document.lasUnidades.unid_metro.value = valor * 0.0254;
         document.lasUnidades.unid_pie.value = valor * 0.833333;
-        document.lasUnidades.unid_yarda.value = valor * 0.027778 ;
-    }else if (id=="yarda "){
+        document.lasUnidades.unid_yarda.value = valor * 0.027778;
+    } else if (id == "yarda ") {
         document.lasUnidades.unid_pulgada.value = valor * 36;
-        document.lasUnidades.unid_pie.value = valor *  3;
-        document.lasUnidades.unid_metro.value = valor*0.9144 ;
-    }else if (id="pie"){
+        document.lasUnidades.unid_pie.value = valor * 3;
+        document.lasUnidades.unid_metro.value = valor * 0.9144;
+    } else if (id == "pie") {
         document.lasUnidades.unid_pulgada.value = valor * 12;
-        document.lasUnidades.unid_yarda.value = valor *  0.3333;
-        document.lasUnidades.unid_metro.value = valor* 0.3048;
+        document.lasUnidades.unid_yarda.value = valor * 0.3333;
+        document.lasUnidades.unid_metro.value = valor * 0.3048;
 
     }
 }
 
+function convertirGR(id) {
+    var grad, rad;
+    if (id == "grados") {
+        grad = document.getElementById("grados").value;
+        rad = (grad * Math.PI) / 180;
+
+    } else if (id == "radianes") {
+        rad = document.getElementById("radianes").value;
+        grad = (rad * 180) / Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
+
+function mostrarOcultar(valorMo){
+    if(valorMo=="val_mostrar"){
+        document.getElementById("divMO").style.display = 'block'
+    }else if (valorMo=="val_ocultar"){
+        document.getElementById("divMO").style.display= 'none'
+
+    }
+
+}
+function calcularsuma (){
+    var num1, num2;
+
+    num1= Number( document.getElementsByName("sum_num1") [0].value);
+    num2= Number(document.getElementsByName("sum_num2") [0].value);
+    document.getElementsByName("sum_total")[0].value= num1+num2;
+}
+
+function calcularresta(){
+    var num1, num2;
+    num1= Number( document.getElementsByName("res_num1") [0].value);
+    num2= Number(document.getElementsByName("res_num2") [0].value);
+    document.getElementsByName("res_total")[0].value= num1-num2;
+
+}
+function calcularmultiplicacion(){
+    var num1, num2;
+    num1= Number( document.getElementsByName("mul_num1") [0].value);
+    num2= Number(document.getElementsByName("mul_num2") [0].value);
+    document.getElementsByName("mul_total")[0].value= num1*num2;
+
+}
+function calculardivision(){
+    var num1, num2;
+    num1= Number( document.getElementsByName("div_num1") [0].value);
+    num2= Number(document.getElementsByName("div_num2") [0].value);
+    document.getElementsByName("div_total")[0].value= num1/num2;
+
+}
